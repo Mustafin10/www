1633e57dce98d8f699992ea7d96bbbd2fbe88adb
Mustafin10 <79744075+Mustafin10@users.noreply.github.com>
2021-12-24 01:48:41 +0500

    Добавил файлы первой части первой лр

---
+++ index.php
@@ -0,0 +1,23 @@
+<p> Date and Time:
+<p>  
+<?php 
+    $d=date("d.m.Y  H:i"); 
+    echo($d);
+?>
+<ul>
+        <li><a href="lab-1-1.php">Task 1-2</a></li>
+	<li><a href="lab-1-2.php">Task 1-3</a></li> 
+	<li><a href="lab-1-3.php">Task 1-4</a></li>
+	<li><a href="lab1-5.php">Task 1-1</a></li>
+	<li><a href="lab-2-1.php">Task 1-6</a></li> 
+	<li><a href="lab-2-2.php">Task 1-7</a></li>
+	<li><a href="lab-2-3.php">Task 1-8</a></li>
+	<li><a href="lab-2-4.php">Task 1-9</a></li>
+	<li><a href="lab-2-5.php">Task 1-10</a></li>
+	<li><a href="lab-2-6.php">Task 1-11</a></li>
+        <li><a href="lab-2-7.php">Task 1-12</a></li>
+        <li><a href="lab-2-8.php">Task 1-13</a></li>
+        <li><a href="lab-2-9.php">Task 1-14</a></li>
+        <li><a href="lab-2-10.php">Task 1-15</a></li>
+        <li><a href="lab-2-11.php">Task 1-16</a></li>  
+</ul>
---
+++ lab-1-2.php
@@ -0,0 +1,10 @@
+<p> Arithmetic operations:
+<p>
+<?php
+$x=rand(1,10);
+$y=rand(1,10);
+print ($x . '+' . $y . '=' . ($x+$y) . '<br>');
+print ($x . '-' . $y . '=' . ($x-$y) . '<br>');
+print ($x . '*' . $y . '=' . ($x*$y) . '<br>');
+print ($x . '/' . $y . '=' . ($x/$y) . '<br>');
+?>
