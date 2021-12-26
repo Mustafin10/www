5ce35e1bf66bdb550853d4426a82be997bcd7b1f
Mustafin10 <79744075+Mustafin10@users.noreply.github.com>
2021-12-26 18:06:49 +0500

    добавил ссылки на файлы третей ЛР

--- index.php
+++ index.php
@@ -25,5 +25,18 @@
         <li><a href="lab-2-14.php">Task 2-3</a></li>
         <li><a href="lab-2-15.php">Task 2-4</a></li>
         <li><a href="lab-2-16.php">Task 2-5</a></li> 
-	<li><a href="lab-2-17.php">Task 2-6</a></li> 
+	<li><a href="lab-2-17.php">Task 2-6</a></li>
+	<li><a href="f1.html">Task 3-1</a></li>
+        <li><a href="f2.html">Task 3-2</a></li>
+	<li><a href="f3.html">Task 3-3</a></li>
+        <li><a href="f4.html">Task 3-4</a></li>
+        <li><a href="f5.html">Task 3-5</a></li>
+        <li><a href="f6.html">Task 3-6</a></li> 
+	<li><a href="f7.php">Task 3-7</a></li>
+	<li><a href="s3-1.html">Task 3-1sm</a></li>
+        <li><a href="s3-2.php">Task 3-2sm</a></li>
+        <li><a href="s3-3.php">Task 3-3sm</a></li>
+        <li><a href="s3-4.php">Task 3-4sm</a></li>
+        <li><a href="s3-5.php">Task 3-5sm</a></li> 
+		<li><a href="s3-6.html">Task 3-6sm</a></li>
 </ul>
