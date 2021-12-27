f1385aad2dc1b1eb6ccb75a577d499bbdd650cfd
Mustafin10 <79744075+Mustafin10@users.noreply.github.com>
2021-12-27 19:22:55 +0500

    Update s3_1.php

--- s3_1.php
+++ s3_1.php
@@ -1,10 +1,12 @@
-<?php
- if ($_POST["a"]>$_POST["b"])  
-   echo ("Largest number:".$_POST["a"]); 
-  elseif ($_POST["a"]<$_POST["b"])  
- echo ("Largest number:".$_POST["b"]);  
- elseif ($_POST["a"]=$_POST["b"])
- echo ("The numbers are equal");
+<title> Мустафин Даниил ПИ-322</title>
+<?
+ $a=$_POST["a"];
+ $b=$_POST["b"];
+  if ($a>=$b) {
+    if ($a==$b) { 
+      echo 'Числа равны';  
+   } else { echo ('Число a=' . $a); }
+  } else { echo ('Число b=' . $b); }
 
- echo ("<BR> <A href='s3-1.html'> come back </A>"); 
-?> 
+echo ("<br> <a href='s3-1.html'> Вернуться назад </a>");
+?>
