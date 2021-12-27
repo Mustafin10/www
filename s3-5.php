bccdee9ce14cd3eaea948331f638029d8c05fb25
Mustafin10 <79744075+Mustafin10@users.noreply.github.com>
2021-12-27 19:19:32 +0500

    Update s3-5.php

--- s3-5.php
+++ s3-5.php
@@ -1,9 +1,9 @@
-<title> Mustafin Daniil PI-322</title>
+<title> Мустафин Даниил ПИ-322</title>
 <body>
-<h3> Profile: Your character </h3>
+<h3> Анкета: Ваш характер </h3>
 
 <form method="POST" action="<?php print $PHP_SELF ?>">
-<p>Your name: <input type="text" name="name" required>
+<p>Ваше имя: <input type="text" name="name" required>
 
 <?
 $name = $_POST["name"];
@@ -31,73 +31,73 @@ if (isset($_POST["obr"])) {
   if ($_POST["v19"]=="y") { $rez++;}
   if ($_POST["v20"]=="n") { $rez++;}
 
-  if ($rez < 8) { echo $name . ', You can sympathize with your friends'; }
-  elseif ($rez > 15) { echo $name . ', you have an agreeable character'; }
-  else { echo $name . ', You are not without flaws, but you can get along with'; }
+  if ($rez < 8) { echo $name . ', Вашим друзьям можно посочувствовать'; }
+  elseif ($rez > 15) { echo $name . ', у Вас покладистый характер'; }
+  else { echo $name . ', Вы не лишены недостатков, но с Вами можно ладить'; }
 }
 
 ?>
 
 
-<p> 1. Do you think that many of your acquaintances have a good character? <br>
-    <input type="radio" name="v1" value="y" checked /> Yes
-    <input type="radio" name="v1" value="n" /> No <br>
-<p> 2. Are you annoyed by small daily chores? <br>
-    <input type="radio" name="v2" value="y" checked /> Yes
-    <input type="radio" name="v2" value="n" /> No <br>
-<p> 3. Do you believe that your friends are devoted to you? <br>
-    <input type="radio" name="v3" value="y" checked /> Yes
-    <input type="radio" name="v3" value="n" /> No <br>
-<p> 4. Is it unpleasant for you when a stranger makes a remark to you? <br>
-    <input type="radio" name="v4" value="y" checked /> Yes
-    <input type="radio" name="v4" value="n" /> No <br>
-<p> 5. Are you capable of hitting a dog or cat? <br>
-    <input type="radio" name="v5" value="y" checked /> Yes
-    <input type="radio" name="v5" value="n" /> No <br>
-<p> 6. Do you often take medications?  <br>
-    <input type="radio" name="v6" value="y" checked /> Yes
-    <input type="radio" name="v6" value="n" /> No <br>
-<p> 7. Do you often change the store you go to for groceries?  <br>
-    <input type="radio" name="v7" value="y" checked /> Yes
-    <input type="radio" name="v7" value="n" /> No <br>
-<p> 8. Do you continue to defend your point of view, realizing that you were wrong? <br>
-    <input type="radio" name="v8" value="y" checked /> Yes
-    <input type="radio" name="v8" value="n" /> No <br>
-<p> 9. Are you burdened with social responsibilities? <br>
-    <input type="radio" name="v9" value="y" checked /> Yes
-    <input type="radio" name="v9" value="n" /> No <br>
-<p> 10. Are you able to wait more than 5 minutes without worrying?  <br>
-    <input type="radio" name="v10" value="y" checked /> Yes
-    <input type="radio" name="v10" value="n" /> No <br>
-<p> 11. Do you often think of your bad luck?  <br>
-    <input type="radio" name="v11" value="y" checked /> Yes
-    <input type="radio" name="v11" value="n" /> No <br>
-<p> 12. Have you preserved your figure in comparison with the past?  <br>
-    <input type="radio" name="v12" value="y" checked /> Yes
-    <input type="radio" name="v12" value="n" /> No <br>
-<p> 13. Can you accept teasing friends with a smile?  <br>
-    <input type="radio" name="v13" value="y" checked /> Yes
-    <input type="radio" name="v13" value="n" /> No <br>
-<p> 14. Do you like family life?  <br>
-    <input type="radio" name="v14" value="y" checked /> Yes
-    <input type="radio" name="v14" value="n" /> No <br>
-<p> 15. Are you vindictive?   <br>
-    <input type="radio" name="v15" value="y" checked /> Yes
-    <input type="radio" name="v15" value="n" /> No <br>
-<p> 16. Do you find that the weather is typical for the given season?  <br>
-    <input type="radio" name="v16" value="y" checked /> Yes
-    <input type="radio" name="v16" value="n" /> No <br>
-<p> 17. Do you happen to be in a bad mood in the morning? <br>
-    <input type="radio" name="v17" value="y" checked /> Yes
-    <input type="radio" name="v17" value="n" /> No <br>
-<p> 18. Is modern painting annoying you? <br>
-    <input type="radio" name="v18" value="y" checked /> Yes
-    <input type="radio" name="v18" value="n" /> No <br>
-<p> 19. Do you get bored with the presence of other people's children in the house for more than one hour? <br>
-    <input type="radio" name="v19" value="y" checked /> Yes
-    <input type="radio" name="v19" value="n" /> No <br>
-<p> 20. Are you tired of doing PHP labs?  <br>
-    <input type="radio" name="v20" value="y" checked /> Yes
-    <input type="radio" name="v20" value="n" /> No <br>
-<P> <INPUT type="submit" name="obr" value="Find out the result">
+<p> 1. Считаете ли Вы, что у многих ваших знакомых хороший характер? <br>
+    <input type="radio" name="v1" value="y" checked /> Да
+    <input type="radio" name="v1" value="n" /> Нет <br>
+<p> 2. Раздражают ли Вас мелкие повседневные обязанности? <br>
+    <input type="radio" name="v2" value="y" checked /> Да
+    <input type="radio" name="v2" value="n" /> Нет <br>
+<p> 3. Верите ли Вы, что ваши друзья преданы Вам? <br>
+    <input type="radio" name="v3" value="y" checked /> Да
+    <input type="radio" name="v3" value="n" /> Нет <br>
+<p> 4. Неприятно ли Вам, когда незнакомый человек делает Вам замечание? <br>
+    <input type="radio" name="v4" value="y" checked /> Да
+    <input type="radio" name="v4" value="n" /> Нет <br>
+<p> 5. Способны ли Вы ударить собаку или кошку?  <br>
+    <input type="radio" name="v5" value="y" checked /> Да
+    <input type="radio" name="v5" value="n" /> Нет <br>
+<p> 6. Часто ли Вы принимаете лекарства?  <br>
+    <input type="radio" name="v6" value="y" checked /> Да
+    <input type="radio" name="v6" value="n" /> Нет <br>
+<p> 7. Часто ли Вы меняете магазин, в который ходите за продуктами?  <br>
+    <input type="radio" name="v7" value="y" checked /> Да
+    <input type="radio" name="v7" value="n" /> Нет <br>
+<p> 8. Продолжаете ли Вы отстаивать свою точку зрения, поняв, что ошиблись? <br>
+    <input type="radio" name="v8" value="y" checked /> Да
+    <input type="radio" name="v8" value="n" /> Нет <br>
+<p> 9. Тяготят ли Вас общественные обязанности?  <br>
+    <input type="radio" name="v9" value="y" checked /> Да
+    <input type="radio" name="v9" value="n" /> Нет <br>
+<p> 10. Способны ли Вы ждать более 5 минут, не проявляя беспокойства?  <br>
+    <input type="radio" name="v10" value="y" checked /> Да
+    <input type="radio" name="v10" value="n" /> Нет <br>
+<p> 11. Часто ли Вам приходят в голову мысли о Вашей невезучести?  <br>
+    <input type="radio" name="v11" value="y" checked /> Да
+    <input type="radio" name="v11" value="n" /> Нет <br>
+<p> 12. Сохранилась ли у Вас фигура по сравнению с прошлым?  <br>
+    <input type="radio" name="v12" value="y" checked /> Да
+    <input type="radio" name="v12" value="n" /> Нет <br>
+<p> 13. Можете ли Вы с улыбкой воспринимать подтрунивание друзей?  <br>
+    <input type="radio" name="v13" value="y" checked /> Да
+    <input type="radio" name="v13" value="n" /> Нет <br>
+<p> 14. Нравится ли Вам семейная жизнь?  <br>
+    <input type="radio" name="v14" value="y" checked /> Да
+    <input type="radio" name="v14" value="n" /> Нет <br>
+<p> 15. Злопамятны ли Вы?   <br>
+    <input type="radio" name="v15" value="y" checked /> Да
+    <input type="radio" name="v15" value="n" /> Нет <br>
+<p> 16. Находите ли Вы, что стоит погода, типичная для данного времени года?  <br>
+    <input type="radio" name="v16" value="y" checked /> Да
+    <input type="radio" name="v16" value="n" /> Нет <br>
+<p> 17. Случается ли Вам с утра быть в плохом настроении? <br>
+    <input type="radio" name="v17" value="y" checked /> Да
+    <input type="radio" name="v17" value="n" /> Нет <br>
+<p> 18. Раздражает ли Вас современная живопись? <br>
+    <input type="radio" name="v18" value="y" checked /> Да
+    <input type="radio" name="v18" value="n" /> Нет <br>
+<p> 19. Надоедает ли Вам присутствие чужих детей в доме более одного часа? <br>
+    <input type="radio" name="v19" value="y" checked /> Да
+    <input type="radio" name="v19" value="n" /> Нет <br>
+<p> 20. Надоедает ли Вам делать лабораторные по PHP?  <br>
+    <input type="radio" name="v20" value="y" checked /> Да
+    <input type="radio" name="v20" value="n" /> Нет <br>
+<P> <INPUT type="submit" name="obr" value="Узнать результат">
 </form>
