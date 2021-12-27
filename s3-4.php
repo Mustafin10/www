0be77ba12898cde61545adfd8978bf4a1414c157
Mustafin10 <79744075+Mustafin10@users.noreply.github.com>
2021-12-27 19:18:48 +0500

    Update s3-4.php

--- s3-4.php
+++ s3-4.php
@@ -1,25 +1,26 @@
-<title> Mustafin Daniil PI-322</title>
+<title> Мустафин Даниил ПИ-322</title>
 <body>
-<p>Registration check:
+<h3> Задача 3.4 </h3>
+<p>Проверка регистрации:
 <form method="POST" action="<?php print $PHP_SELF ?>">
- <p>Login: <INPUT type="text" name="n">
- <P> <INPUT type="submit" name="obr" value="Check">
+ <p>Логин: <INPUT type="text" name="n">
+ <P> <INPUT type="submit" name="obr" value="Проверить">
 </form>
 
 <?
 $n=$_POST["n"];
 $arr=array();
-$arr[0]="Mustafin Daniil Eduardovic";
-$arr[1]="Petrov Petr Petrovic";
-$arr[2]="Ivanov Ivan Ivanic";
-$arr[3]="Kirillov Kirill Kirillovic";
+$arr[0]="Мустафин Даниил Эдуардович";
+$arr[1]="Петров Петр Петрович";
+$arr[2]="Иванов Иван Иванович";
+$arr[3]="Михайлов Михаил Михайлович";
 if (isset($_POST["obr"])) {
 
   for ($i = 0; $i < count($arr); $i++) {
-    if ($n == $arr[$i]) { echo 'Hello, ' . $n; $acc=false; break;}
+    if ($n == $arr[$i]) { echo 'Здравствуйте, ' . $n; $acc=false; break;}
     else { $acc=true; }
   }
-  if ($acc) { echo 'You are not a registered user!';}
+  if ($acc) { echo 'Вы не зарегистрированный пользователь!';}
 
 }
 
